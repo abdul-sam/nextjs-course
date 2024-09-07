9181ce89b44fd46b4a10fe0e515a12c431db1361
abdul-sam <abdulsamad.3939@gmail.com>
2024-09-08 01:31:36 +0500

    Custom not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1>Page Not Found</h1>
+      <p>Sorry, we couldn't find the page you were looking for.</p>
+    </>
+  )
+}
+
+export default NotFound;
