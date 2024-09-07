99ad72be5e75ff8d7998632c6336bdf082300007
abdul-sam <abdulsamad.3939@gmail.com>
2024-09-08 01:24:33 +0500

    Catch all Segmant

---
+++ app/docs/[...slug]/page.tsx
@@ -0,0 +1,16 @@
+const Docs = ({ params }: {
+  params: {
+    slug: string[]
+  }
+}) => {
+
+  if (params.slug.length === 1) {
+    return <div>Docs for {params.slug[0]}</div>
+  } else if (params.slug.length === 2) {
+    return <div>Docs for {params.slug[0]} in {params.slug[1]}</div>
+  }
+  return <div>Docs</div>
+}
+
+
+export default Docs;
