46540371d7121fec07a541d3f8ad90b0f1c84ebf
abdul-sam <abdulsamad.3939@gmail.com>
2024-09-07 09:08:30 +0500

    dynamic routes

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,12 @@
+export default function ProductDetails( {
+  params 
+}: { 
+  params: { productId: string } 
+} ) {
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Product ID: {params.productId}</p>
+    </div>
+  );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,18 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        <li>
+          <a>Product 1</a>
+        </li>
+        <li>
+          <a>Product 2</a>
+        </li>
+        <li>
+          <a>Product 3</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
