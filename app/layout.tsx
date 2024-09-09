d2e583f88332483d64338a5c40cbc3e9cb060ddb
abdul-sam <abdulsamad.3939@gmail.com>
2024-09-09 11:25:22 +0500

    covered layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header style={{ 
+          backgroundColor: 'lightblue',
+          padding: '1rem'
+          }}
+        >
+          This is the header
+        </header>
         {children}
+
+        <footer style={{ 
+          backgroundColor: 'ghostwhite',
+          padding: '1rem'
+          }}
+        >
+          This is the footer
+        </footer>
       </body>
     </html>
   );
