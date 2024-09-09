44d17bf134183aadb0d04856980f4a31f1229463
abdul-sam <abdulsamad.3939@gmail.com>
2024-09-10 01:12:15 +0500

    dynamic metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,8 +1,26 @@
-export default function ProductDetails( {
-  params 
-}: { 
-  params: { productId: string } 
-} ) {
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  }
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  // Use async to fetch data from an API 
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`Product Details - ${params.productId}`);
+    }, 1000);
+  });
+
+  return {
+    title: `${title}`,
+  };
+}
+
+
+export default function ProductDetails( { params }: Props) {
   return (
     <div>
       <h1>Product Details</h1>
