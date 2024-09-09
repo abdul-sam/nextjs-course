4dc3830d9f8ad0df782b6e3a9ed9cf82232bef19
abdul-sam <abdulsamad.3939@gmail.com>
2024-09-10 01:01:53 +0500

    static metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'About Page',
+  description: 'This is the About page!',
+};
+
 export default function About() {
   return (
     <h2 className="text-5xl font-bold">This is the About page!</h2>
